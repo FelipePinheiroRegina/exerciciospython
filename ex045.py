753910cedc94b82e5222f934f13d8bc5722faa0f
Felipe Pinheiro <flppinheiro@hotmail.com>
2023-04-09 00:34:51 -0300

    prof-ex045

--- ex045.py
+++ ex045.py
@@ -1,9 +1,13 @@
+# Critérios
 '''
 elabore um jogo que faca o computador jogar
 jokenpô com você
 
     CRITERIOS
 - Pedra, papel, tesoura
+'''
+
+# Meu código de JokenPO
 '''
 from random import choice
 po1 = 'pedra'
@@ -47,5 +51,53 @@ elif jogador == 'pedra' and comp == 'tesoura':
     print('Computador escolheu {}'.format(comp.capitalize()))
     print('Pedra quebra Tesoura!')
     print('Jogador K.O!')
+'''
 
+# Código do Professor Guanabis
 
+from random import randint
+from time import sleep
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+print('\033[4;36m{:=^20}\033[m'.format(' JOKENPO '))
+print('''[ 0 ] Pedra
+[ 1 ] Papel
+[ 2 ] Tesoura ''')
+jogador = int(input('Qual sua escolha? '))
+print('JOOOO')
+sleep(1)
+print('KEEENN')
+sleep(1)
+print('PÔ!')
+print('\033[1;31m=-\033[m'*15)
+print('Computador escolheu {}'.format(itens[computador]))
+print('Jogador escolheu {}'.format(itens[jogador]))
+print('\033[1;31m=-\033[m'*15)
+
+if computador == 0: # Computador jogou Pedra
+    if jogador == 0:
+        print('IMPATE!')
+    elif jogador == 1:
+        print('JOGADOR VENCEU!')
+    elif jogador == 2:
+        print('COMPUTADOR VENCEU!')
+    else:
+        print('JOGADA INVALIDA, TENTE NOVAMENTE!')
+elif computador == 1: # Computador jogou Papel
+        if jogador == 0:
+            print('COMPUTADOR VENCEU!')
+        elif jogador == 1:
+            print('IMPATE!')
+        elif jogador == 2:
+            print('JOGADOR VENCEU!')
+        else:
+            print('JOGADA INVALIDA, TENTE NOVAMENTE!')
+elif computador == 2: # Computador jogou Tesoura
+        if jogador == 0:
+            print('JOGADOR VENCEU!')
+        elif jogador == 1:
+            print('COMPUTADOR VENCEU!')
+        elif jogador == 2:
+            print('IMPATE!')
+        else:
+            print('JOGADA INVALIDA, TENTE NOVAMENTE!')
