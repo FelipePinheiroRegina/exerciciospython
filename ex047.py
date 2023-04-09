6a1f6ab72a7811ab9682cb7873966519001d5bd9
Felipe Pinheiro <flppinheiro@hotmail.com>
2023-04-09 14:56:03 -0300

    exercicio 047 respondido

--- ex047.py
+++ ex047.py
@@ -1,2 +1,5 @@
 ''' crie um programa que mostre na tela todos
 os números pares que estão no intervalo entre 1 e 50'''
+for c in range(1, 51):
+    if c % 2 == 0:
+        print('\033[1;32m', c, end=', \033[m')
