28e2261846ff06c79be8d06ab1eb90e26fa2a205
Felipe Pinheiro <flppinheiro@hotmail.com>
2023-04-10 22:13:11 -0300

    ex 050 respondido

--- ex050.py
+++ ex050.py
@@ -1,4 +1,10 @@
 '''Desenvolva um programa que leia seis
 número inteiros e mostre a soma apenas
 daqueles que forem pares. se o valor
-digitado fir ímpar, desconsidere-o'''
+digitado for ímpar, desconsidere-o'''
+somapar = 0
+for c in range(1, 6 + 1):
+    n = int(input('Número: '))
+    if n % 2 == 0:
+        somapar += n
+print('Somando todos valores pares, deu {}'.format(somapar))
