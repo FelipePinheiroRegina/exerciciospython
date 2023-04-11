b81d149c353ee8ac9154d4da2123c0add9aae448
Felipe Pinheiro <flppinheiro@hotmail.com>
2023-04-10 22:06:12 -0300

    ex 049 respondido

--- ex049.py
+++ ex049.py
@@ -1,3 +1,6 @@
 '''refaça o desafio 009, mostrando
 a tabuada de um número que o usuario
 escolher, só que agora utilizando um laço for'''
+n = int(input('Tabuada: '))
+for c in range(1, 10 + 1):
+    print('\033[4;34m{} x {:>2} = {:>2}\033[m'.format(n, c, n * c))
