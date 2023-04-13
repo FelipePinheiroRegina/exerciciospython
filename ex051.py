78ac7bb658c20d5da1b0fa0ff779024d2d253b9c
Felipe Pinheiro <flppinheiro@hotmail.com>
2023-04-12 22:18:13 -0300

    ex-051-respondido

--- ex051.py
+++ ex051.py
@@ -1,3 +1,28 @@
 '''Desenvolva um programa que leia o
 primeiro termo e a razão de uma PA. no final, mostre
 termos dessa progressão'''
+print('='*10)
+print('{:^10}'.format('P.A'))
+print('='*10)
+termo = int(input('Qual é o Primeiro Termo, (a1), da P.A: '))
+razao = int(input('Qual é a razão da PA: '))
+if razao > 0:
+    for c in range(1, 5 + 1):
+        print(termo, end=', ')
+        termo += razao
+    print('\n(P.A CRESCENTE)')
+elif razao == 0:
+    for c in range(1, 5 + 1):
+        termo = termo
+        print(termo, end=', ')
+    print('\n(P.A CONSTANTE)')
+else:
+    for c in range(1, 5 + 1):
+        print(termo, end=', ')
+        termo += razao
+
+    print('\n(P.A DECRESCENTE)')
+
+
+
+
