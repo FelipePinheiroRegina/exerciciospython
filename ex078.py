92dfe6dddf15e26bbb063213f924a1b0d6d5ef4a
Felipe Pinheiro <flppinheiro@hotmail.com>
2023-05-07 22:10:18 -0300

    praticando

--- ex078.py
+++ ex078.py
@@ -9,7 +9,21 @@ na lista.
 Valores = list()
 Maior = 0
 Menor = 0
+posiçãom = 0
+posiçãor = 0
 for cont in range(1, 5 + 1):
     Valores.append(int(input(f'Entre com o {cont}° Valor: ')))  # Pedindo para o usuário entrar com 5 valores
-
 for p, v in enumerate(Valores):  # Analisando
+    if p == 0:
+        Maior = Valores[p]
+        Menor = Valores[p]
+    if Valores[p] > Maior:
+        Maior = Valores[p]
+        posiçãom = p
+    if Valores[p] < Menor:
+        Menor = Valores[p]
+        posiçãor = p
+print(f'A Lista digitada foi: {Valores}')
+print(f'O maior valor digitado foi {Maior} na posição {posiçãom}!')
+print(f'O menor valor digitado foi {Menor} na posição {posiçãor}!')
+
--- ex079.py
+++ ex079.py
@@ -1,8 +1,23 @@
 '''
 Crie um programa onde o usuário possa
-digitar vários valores numéricos e cadastr-os
+digitar vários valores numéricos e cadastre-os
 em uma lista, Caso o número já exista la
 dentro, ele não será adicionado,
 no final, serão exibidos todos os valores
 únicos digitados em ordem crescente
 '''
+Valores = list()
+R = ''
+while True:
+    Valores.append(int(input('Digite um valor: ')))
+    for v in Valores:
+        if v == Valores:
+            print('Valor Repetido!, Digite novamente!')
+            Valores.append(int(input('Digite outro valor: ')))
+        else:
+            print('Valor Registrado!')
+    R = str(input('Quer continuar? [ S / N ]: '))
+    if R in 'Nn':
+        break
+
+
