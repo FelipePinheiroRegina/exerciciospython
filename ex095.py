82f1d7589971261cb26f8833f5b2537f5e0a5762
Felipe Pinheiro <flppinheiro@hotmail.com>
2023-06-09 21:31:38 -0300

    ex 95

--- ex095.py
+++ ex095.py
@@ -20,10 +20,23 @@ while True:
         break
 
 print('-=' * 30)
-print(f'{"Cod":<3} {"Nome":<10} {"Gols":<10} {"Total":>8}')
+print(f'{"Cod":>4} {"Nome":<15} {"Gols":<15} {"Total":<15}')
 print('--' * 26)
-for i, j in enumerate(time):
-    print(f"{i:<3} {j['nome']:<10} {j['gols'][0:]:<10} {j['total']:>8}")
+for i, v in enumerate(time):
+    print(f'{i:>4}', end=' ')
+    for d in v.values():
+        print(f'{str(d):<15}', end=' ')
+    print()
+print('-=' * 30)
+while True:
+    opçao = int(input('Mostrar dados de qual jogador? (999 para parar): '))
+    if opçao == 999:
+        break
+    elif opçao <= len(time):
+        print(f'    ---LEVANTAMENTO DO JOGADOR {time[opçao]["nome"]}')
+        for i, p in enumerate(time[opçao]['gols']):
+            print(f'    No jogo {i+1} fez {p} gols.')
+    print('-' * 26)
 
 
 
