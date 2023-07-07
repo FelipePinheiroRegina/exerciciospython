0b16b93c08855c8183c48e0629ad45078812359c
Felipe Pinheiro <flppinheiro@hotmail.com>
2023-07-06 21:23:21 -0300

    ex113

--- ex113.py
+++ ex113.py
@@ -1,13 +1,29 @@
-try:
-    núm_inteiro = int(input('Entre com um número inteiro: '))
-except (TypeError, ValueError):
+def LeiaInt(Verificar):
     while True:
-        print(f'ERRO, por favor digite um número inteiro válido!')
-        núm_inteiro = str(input('Entre com um número inteiro: ')).strip()
-        if núm_inteiro.isnumeric() or núm_inteiro != '':
-            núm_inteiro = int
-            break
-else:
-    print(f'O número inteiro foi {núm_inteiro}')
+        try:
+            resp = int(input(Verificar))
+        except (ValueError, TypeError):
+            print('\033[31mERRO, Por favor, entre com um número inteiro.\033[m')
+            continue
+        else:
+            return resp
+
+
+def LeiaFloat(Verificar):
+    while True:
+        try:
+            resp = float(input(Verificar))
+        except (ValueError, TypeError):
+            print('\033[31mERRO, Por favor, entre com um número real.\033[m')
+            continue
+        else:
+            return resp
+
+
+núm = LeiaInt('Entre com um inteiro: ')
+númf = LeiaFloat('Entre com um real: ')
+print(f'Número Inteiro = [  \033[32m{núm}\033[m  ]')
+print(f'Número Real = [  \033[36m{númf}\033[m  ]')
+
 
 
